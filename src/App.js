323b51fa72c183558a237737d9f61a7057914f9f
Anicia Tarlao <tarlaoanicia@gmail.com>
2019-10-30 13:41:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Answer from './components/Answer.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies = null,
+    };
+  }
+
+  componentDidMount() {
+    this.getMovie();
+  }
+
+  getMovie = () => {
+    axios
+      .get("https://hackathon-wild-hackoween.herokuapp.com/movies")
+      .then(response => response.data)
+      .then(data => {
+        this.setState({
+          movie: data
+        });
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Answer />
+      </div>
+    );
+  }
 }
 
 export default App;
