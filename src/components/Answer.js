2d8c0f6bdc3bbf0d649b777ad2388528fb2f50cd
Anicia Tarlao <tarlaoanicia@gmail.com>
2019-10-30 14:58:36 +0100

    answer ok

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,16 +1,28 @@
 import React from 'react';
 
 
+const displayMovieStyle = {
+    display: "flex",
+    justifyContent: "space-around",
+    fontSize: "22pt",
+    margin: "50px",
+}
+
+const movieInfoStyle = {
+    marginTop: "100px",
+}
+
+
 function Answer({movies}) {
     return (
-        <div className="DisplayMovie">
-            <img src={movies.posterUrl} />
-            <p>Title: {movies.title} {' '}</p>
-            <p>Director: {movies.director}</p>
-            <p>Year: {movies.year}</p>
-            <p>Country: {movies.country}</p>
-         
-        
+        <div className="DisplayMovie" style={displayMovieStyle}>
+            <div className="MovieInfo" style={movieInfoStyle}>
+                <p>Title: {movies.title}</p>
+                <p>Director: {movies.director}</p>
+                <p>Year: {movies.year}</p>
+                <p>Country: {movies.country}</p>
+            </div>
+            <div className="MoviePoster"><img src={movies.posterUrl} alt="MoviePoster"/></div>
         </div>
     )
 }
